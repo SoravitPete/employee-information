b9b477551ad89a799baf789dfb46757de150485f
SoravitPete <esterpiano.official@gmail.com>
2022-04-27 17:00:54 +0700

    Create CompanyInfo

---
+++ utils/company_information.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from utils.dao.salary_dao import SalaryDao
+from utils.dao.employee_personal_info_dao import EmployeeInfoDao
+
+class CompanyInfo:
+
+
+  def __init__(self, connection_url="sqlite:///sample.db"):
+    engine = create_engine(connection_url)
+    session = sessionmaker(bind=engine)
+    self.__db_session = session()
+
+
+  def employee(self):
+    return EmployeeInfoDao(self.__db_session)
+
+  def salary(self):
+    return SalaryDao(self.__db_session)
